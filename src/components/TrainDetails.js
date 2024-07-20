6205b7e17b0455b10a5ee95a50097df6181e7ab3
AsharaTmel <asharatme@gmail..com>
2024-07-20 15:46:42 +0530

    Initial clientside setup

---
+++ src/components/TrainDetails.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './TrainDetails.css';
+
+const TrainDetails = () => {
+  return (
+    <div className="train-details">
+      <h2>Real-Time Train Details</h2>
+      <p>No data available</p>
+    </div>
+  );
+};
+
+export default TrainDetails;
