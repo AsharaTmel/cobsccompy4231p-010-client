2f4fb6f7e7ea975ff5062c3cea5f777c91fad74e
AsharaTmel <asharatme@gmail..com>
2024-08-08 20:24:46 +0530

    updated

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+// App.js
+import React from 'react';
+import TrainRoutes from './TrainRoutes';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Train Tracking System</h1>
+      <TrainRoutes />
+    </div>
+  );
+};
+
+export default App;
